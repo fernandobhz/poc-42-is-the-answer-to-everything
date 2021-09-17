9caa679dae33bf0f87a096af5c1122750cc7a9e3
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-17 13:12:05 -0300

    testing if 42 is the answert to everything

---
+++ index.js
@@ -0,0 +1,7 @@
+const is42TheAnswertToEverything = Math.random() + Math.random();
+
+if (is42TheAnswertToEverything === 42) {
+  console.log(`At least here is was true`)
+} else {
+  console.log(`hmmm... 42 shouldn't be the answer to everything? here it wasn't`);
+}
